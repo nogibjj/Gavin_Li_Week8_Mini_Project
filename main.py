b6832fb8a6f8da9182799b49115ab118740cb20f
GLi81 <90353988+gli81@users.noreply.github.com>
2023-10-22 22:26:25 +0000

    worked on python

--- main.py
+++ main.py
@@ -2,19 +2,29 @@
 
 # import numpy as np
 import pandas as pd
+import time
+import psutil
 
-def read_titanic():
-    df = pd.read_csv("./dataset/train.csv")
-    return df
+# def read_titanic():
+#     df = pd.read_csv("./dataset/train.csv")
+#     return df
 
-def general_get_desc(path: "str") -> "pd.DataFrame":
-    return pd.read_csv(path).describe()
+# def general_get_desc(path: "str") -> "pd.DataFrame":
+#     return pd.read_csv(path).describe()
 
 
 def main():
     # test = get_desc_stats()
     # print(test.loc["count", "PassengerId"])
-    pass
+    start_time = time.time()
+    df = pd.read_csv("./dataset/train.csv")
+    print(f"Mean of Survived column (survival rate) is: {df["Survived"].mean()}")
+    end_time = time.time()
+    print(f"Time used: {end_time - time: /4f} seconds")
+    print(f"CPU usage: {psutil.cpu_percent()}")
+    print(f"CPU usage: {psutil.virtual_memory()}")
+    return 0
+
 
 if __name__ == "__main__":
     main()
--- test_main.py
+++ test_main.py
@@ -1,39 +1,40 @@
 # -*- coding: utf-8 -*-
 
-from main import read_titanic#, general_get_desc
-import matplotlib.pyplot as plt
+from main import main#read_titanic#, general_get_desc
+# import matplotlib.pyplot as plt
 
-def test_get_desc_stats():
+def test_main():
     # print(longestSubstring("abcdcca"))
-    assert read_titanic()\
-        .describe(include="all").\
-        loc["count", "PassengerId"] == 891
-    assert round(read_titanic().\
-        describe(include="all").\
-        loc["mean", "Survived"], 4) == 0.3838
+    # assert read_titanic()\
+    #     .describe(include="all").\
+    #     loc["count", "PassengerId"] == 891
+    # assert round(read_titanic().\
+    #     describe(include="all").\
+    #     loc["mean", "Survived"], 4) == 0.3838
+    assert main() == 0
 
 
 if __name__ == "__main__":
-    test_get_desc_stats()
-    ## TODO generate summary statistics
-    df = read_titanic()
-    print(
-        "Below are some summary statistics of variable `Survived` in the Titanic dataset"
-    )
-    smm = df.describe(include="all")
-    di = {
-        "mean"                  :   smm.loc["mean", "Survived"],
-        "median"                :   df["Survived"].median(),
-        "standard deviation"    :   smm.loc["std", "Survived"],
-        "maximum"               :   smm.loc["max", "Survived"],
-        "minimum"               :   smm.loc["min", "Survived"]}
-    for k, v in di.items():
-        print(f"{k} : {v}")
+    # test_get_desc_stats()
+    # ## TODO generate summary statistics
+    # df = read_titanic()
+    # print(
+    #     "Below are some summary statistics of variable `Survived` in the Titanic dataset"
+    # )
+    # smm = df.describe(include="all")
+    # di = {
+    #     "mean"                  :   smm.loc["mean", "Survived"],
+    #     "median"                :   df["Survived"].median(),
+    #     "standard deviation"    :   smm.loc["std", "Survived"],
+    #     "maximum"               :   smm.loc["max", "Survived"],
+    #     "minimum"               :   smm.loc["min", "Survived"]}
+    # for k, v in di.items():
+    #     print(f"{k} : {v}")
 
-    ## TODO one visualization
-    print("Below is a histogram of variable `Survived` in the Titanic dataset")
-    plt.hist(df["Survived"])
-    plt.title("Histogram of Survived")
-    plt.xlabel("Survived")
-    plt.ylabel("Count")
-    plt.show()
+    # ## TODO one visualization
+    # print("Below is a histogram of variable `Survived` in the Titanic dataset")
+    # plt.hist(df["Survived"])
+    # plt.title("Histogram of Survived")
+    # plt.xlabel("Survived")
+    # plt.ylabel("Count")
+    # plt.show()
